8643f9389a707030df507e0f16245ff8ee5a9072
Alessandro Marotta <alessandro.marotta@samag.tech>
2023-01-18 11:43:55 +0100

    Aggiunta eccezione

---
+++ src/Exceptions/HandlerNotFoundException.php
@@ -0,0 +1,14 @@
+<?php namespace SamagTech\SimpleAppBridge\Exceptions;
+
+use Exception;
+
+/**
+ * Eccezione per handler non trovato
+ *
+ */
+class HandlerNotFoundException extends Exception {
+
+    public function __construct($message = 'Handler not found') {
+        parent::__construct($message);
+    }
+}
