f39380877f345f81149fa1e38aca338c100820c3
Alessandro Marotta <alessandro.marotta@samag.tech>
2023-01-18 11:44:07 +0100

    Definizione classe application

--- src/Application.php
+++ src/Application.php
@@ -1,12 +1,54 @@
 <?php namespace SamagTech\SimpleAppBridge;
 
+use Bref\Event\Handler;
 use Dotenv\Dotenv;
+use SamagTech\SimpleAppBridge\Exceptions\HandlerNotFoundException;
 
 class Application {
 
+    private ?string $handler = null;
+
+    //-----------------------------------------------------------------------
+
+    /**
+     * Costruttore.
+     *
+     * @param string $env   Path del file .env per caricare l'environment
+     */
     public function __construct(string $env) {
 
         $dotenv = Dotenv::createImmutable($env);
         $dotenv->load();
     }
+
+    //-----------------------------------------------------------------------
+
+    /**
+     * Imposta l'handler tramite namespace
+     *
+     * @param string $handler
+     *
+     * @return void
+     */
+    public function setHandler(string $handler) : void {
+        $this->handler = $handler;
+    }
+
+    //-----------------------------------------------------------------------
+
+    /**
+     * Esegue l'hander impostato
+     *
+     * @return \Bref\Event\Handler
+     */
+    public function run() : Handler {
+
+        if ( is_null($this->handler) ) {
+            throw new HandlerNotFoundException();
+        }
+
+        return new $this->handler;
+    }
+
+    //-----------------------------------------------------------------------
 }
